9b4328a1a2189d56817d3b7d5689190c5812d370
jessicamflemingc <114246917+jessicamflemingc@users.noreply.github.com>
2023-01-22 16:48:36 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5,
-    center: { lat: 46.13850108, lng: -81.97709343 },
+    center: { lat: 46, lng: -81 },
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
