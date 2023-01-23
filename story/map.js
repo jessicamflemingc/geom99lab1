bdf7c4c067cf4404c83dd86e2b2ebbc954832f6e
jessicamflemingc <114246917+jessicamflemingc@users.noreply.github.com>
2023-01-23 09:53:51 -0500

    Update map.js

--- story/map.js
+++ story/map.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 19.542, lng: 155.665 },
     zoom: 8,
   });
 }
