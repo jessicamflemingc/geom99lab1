74563ea1638ba487117f4eb4c5e64a230447e220
jessicamflemingc <114246917+jessicamflemingc@users.noreply.github.com>
2023-01-23 15:13:59 -0500

    Update map.js

--- story/map.js
+++ story/map.js
@@ -8,10 +8,41 @@ function initMap() {
   const loihi = { lat: 18.914, lng: -155.271 };
   const hualalai = { lat: 19.686, lng: -155.865 };
   const kilauea = { lat: 19.406, lng: -155.283 };
-  const maunakea = { lat: 19.820, lng: -155.468 };
+  const uluru = { lat: 19.820, lng: -155.468 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: mapcentre,
+  });
+const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km "</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Uluru",
+  });
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map,
+    title: "Uluru (Ayers Rock)",
+  });
+
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
   });
 
 new google.maps.Marker({
