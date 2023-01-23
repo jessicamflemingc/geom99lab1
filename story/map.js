a34885648afc912d64fc0bae43d2dc93b6c727e5
jessicamflemingc <114246917+jessicamflemingc@users.noreply.github.com>
2023-01-23 12:05:16 -0500

    Update map.js

--- story/map.js
+++ story/map.js
@@ -13,19 +13,49 @@ setMarkers(map);
 
 // Data for the markers consisting of a name, a LatLng and a zIndex for the
 // order in which these markers should display on top of each other.
-//const volcano = [
-//  ["Haleakalā", 20.709, -156.253],
-//  ["Mauna Loa", 19.471, -155.592],
-//  ["Lōʻihi", 18.914, -155.271],
-//  ["Hualālai", 19.686, -155.865],
-//  ["Kīlauea", 19.406, -155.283],
+const volcano = [
+  ["Haleakalā", 20.709, -156.253],
+  ["Mauna Loa", 19.471, -155.592],
+  ["Lōʻihi", 18.914, -155.271],
+  ["Hualālai", 19.686, -155.865],
+  ["Kīlauea", 19.406, -155.283],
 ];
-new google.maps.Marker({
-    position: { lat: 20.602, lng: -157.511 };
-    map,
-    title: "Hello World!",
-  });
+function setMarkers(map) {
+  // Adds markers to the map.
+  // Marker sizes are expressed as a Size of X,Y where the origin of the image
+  // (0,0) is located in the top left of the image.
+  // Origins, anchor positions and coordinates of the marker increase in the X
+  // direction to the right and in the Y direction down.
+  const image = {
+    url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32),
+  };
+  // Shapes define the clickable region of the icon. The type defines an HTML
+  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
+  // The final coordinate closes the poly by connecting to the first coordinate.
+  const shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: "poly",
+  };
 
+  for (let i = 0; i < beaches.length; i++) {
+    const beach = volcano[i];
+
+    new google.maps.Marker({
+      position: { lat: beach[1], lng: beach[2] },
+      map,
+      icon: image,
+      shape: shape,
+      title: beach[0],
+      zIndex: beach[3],
+    });
+  }
 }
 
 window.initMap = initMap;
+
