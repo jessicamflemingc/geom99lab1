78344f0a5016e9493d3da5ef3576e66574bd8f43
jessicamflemingc <114246917+jessicamflemingc@users.noreply.github.com>
2023-01-24 13:40:03 -0500

    Update map.js

--- story/map.js
+++ story/map.js
@@ -2,9 +2,63 @@ let map;
 
 function initMap() {
   const mapcentre = { lat: 20.602, lng: -157.511 };
+  const haleakala = { lat: 20.709, lng: -156.253 };
+  const maunaloa = { lat: 19.471, lng: -155.592 };
+  const loihi = { lat: 18.914, lng: -155.271 };
+  const hualalai = { lat: 19.686, lng: -155.865 };
+  const kilauea = { lat: 19.406, lng: -155.283 };
+  const maunakea = { lat: 19.820, lng: -155.468 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: mapcentre,
+  });
+  new google.maps.Marker({
+    position: haleakala,
+    map,
+    title: "Haleakalā Volcano",
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+    }
+  });  
+new google.maps.Marker({
+    position: maunaloa,
+    map,
+    title: "Mauna Loa Volcano",
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+    }
+  });
+new google.maps.Marker({
+    position: loihi,
+    map,
+    title: "Kamaʻehuakanaloa Seamount (formerly Lōʻihi",
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+    }
+  });
+new google.maps.Marker({
+    position: hualalai,
+    map,
+    title: "Hualālai Volcano",
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+    }
+  });
+new google.maps.Marker({
+    position: kilauea,
+    map,
+    title: "Kīlauea",
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+    }
+  });
+new google.maps.Marker({
+    position: maunakea,
+    map,
+    title: "Mauna Kea",
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
+    }
   });
 }
 window.initMap = initMap;
