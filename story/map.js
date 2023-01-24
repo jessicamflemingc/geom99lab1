f2f6a9d2d29d7c4ed232c9474b352a8d005f0bae
jessicamflemingc <114246917+jessicamflemingc@users.noreply.github.com>
2023-01-23 19:57:44 -0500

    Update map.js

--- story/map.js
+++ story/map.js
@@ -1,8 +1,14 @@
 function initMap() {
+  const mapcentre = { lat: 20.602, lng: -157.511 };
+  const haleakala = { lat: 20.709, lng: -156.253 };
+  const maunaloa = { lat: 19.471, lng: -155.592 };
+  const loihi = { lat: 18.914, lng: -155.271 };
   const hualalai = { lat: 19.686, lng: -155.865 };
+  const kilauea = { lat: 19.406, lng: -155.283 };
+  const maunakea = { lat: 19.820, lng: -155.468 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 6,
-    center: hualalai,
+    zoom: 8,
+    center: mapcentre,
   });
   const contentString =
     '<div id="content">' +
@@ -35,6 +41,37 @@ function initMap() {
     title: "Hualalai",
   });
 
+    const contentmaunaloa =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+    "(last visited June 22, 2009).</p>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentmauna,
+    ariaLabel: "Maunaloa",
+  });
+  const marker = new google.maps.Marker({
+    position: maunaloa,
+    map,
+    title: "Maunaloa",
+  });
+  
   marker.addListener("click", () => {
     infowindow.open({
       anchor: marker,
