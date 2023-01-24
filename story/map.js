7abd1decb1aba4fcc1fc570b7076e16af144a447
jessicamflemingc <114246917+jessicamflemingc@users.noreply.github.com>
2023-01-23 19:26:16 -0500

    Update map.js

--- story/map.js
+++ story/map.js
@@ -1,5 +1,5 @@
 function initMap() {
-  const uluru = { lat: 20.602, lng: -157.511 };
+  const uluru = { lat: -25.363, lng: 131.044 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 6,
     center: uluru,
