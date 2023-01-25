bf37a47e76994de1865e3e316ff6db25a8400784
jessicamflemingc <114246917+jessicamflemingc@users.noreply.github.com>
2023-01-24 20:52:27 -0500

    Update map.js

--- story/map.js
+++ story/map.js
@@ -18,6 +18,7 @@ function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: mapcentre,
+    mapTypeId: 'terrain',
   });
   new google.maps.Marker({
     position: haleakala,
